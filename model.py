c6e7e203a398670eb4639aa4c1609151ccaba754
Jack <therealjackwu@gmail.com>
2025-08-16 20:57:03 -0700

    init model class

---
+++ model.py
@@ -0,0 +1,6 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
